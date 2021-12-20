54fc4cb45f3419a2ef09c2ec631571321ab9b59e
tunchunairarko <tunchunairarko@gmail.com>
2021-12-20 20:20:02 +0000

    Added server as well

---
+++ client/src/ComponentTest.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function ComponentTest() {
+    return (
+        //YOU CAN ADD YOUR COMPONENTS HERE TO TEST
+        <div>
+            <h1>Hello Deadpool</h1>
+            <button>Hello</button>
+        </div>
+    )
+}
